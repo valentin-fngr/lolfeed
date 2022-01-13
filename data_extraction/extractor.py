ff53e92588909bb6dac30324421c7692fdf6aa73
valentin-fngr <valentin.fontanger@gmail.com>
2022-01-13 11:26:59 +0100

    modified the user extractor class

--- data_extraction/extractor.py
+++ data_extraction/extractor.py
@@ -15,30 +15,20 @@ class UserDataExtractor:
         In short, instanciating this class will allow you to at least access summoner name and puuid
     """
 
-    def __init__(self, summoner_name): 
-        if not isinstance(summoner_name, str) and not summoner_name: 
-            raise ValueError("Make sure summoner_name is a string")    
-        self.summoner_name = summoner_name
-        try: 
-            self.puuid = self._retrieve_puuid()
-            self.matches = self._retrieve_matches()
-        except Exception as e: 
-            raise e
-
-    def _retrieve_puuid(self): 
+    def retrieve_puuid(self, summoner_name): 
         """
             retrieve a user's puuid
         """
-        endpoint = PUUIS_ENDPOINT + self.summoner_name
+        endpoint = PUUIS_ENDPOINT + summoner_name
         r = requests.get(endpoint, headers=HEADERS)
         json = r.json()
         return json["puuid"]
     
-    def _retrieve_matches(self): 
+    def retrieve_matches(self, puuid): 
         """
             retrieve a user's matches (ranked only ! )
         """
-        endpoint = MATCHS_ENDPOINTS + f"{self.puuid}/ids?type=ranked"
+        endpoint = MATCHS_ENDPOINTS + f"{puuid}/ids?type=ranked"
         r = requests.get(endpoint, headers=HEADERS)
         return r.json()
          
@@ -47,16 +37,9 @@ class MatchDataExtractor:
     """
         A class responsible for extracting a match informations
     """
-
-    def __init__(self, match_id): 
-        
-        if not isinstance(match_id, str) and not match_id: 
-            raise ValueError("Please make sure to provide a correct match_id")
-        self.match_id = match_id 
-        self.match_content = self.retrieve_match_content() 
-
-    def _retrieve_match_content(self): 
-        endpoint = MATCH_DETAIL_ENDPOINTS + self.match_id
+    
+    def retrieve_match_content(self, match_id): 
+        endpoint = MATCH_DETAIL_ENDPOINTS + match_id
         r = requests.get(endpoint, headers=HEADERS)
         return r.json()
         
---
+++ tools/runapp.py
@@ -0,0 +1,17 @@
+from data_extraction import extractor
+
+
+
+
+
+def main(): 
+    """
+        entry point
+    """
+    
+    user_extractor = extractor.UserDataExtractor()
+    
+
+
+if __name__ == "__main__":
+    main()
